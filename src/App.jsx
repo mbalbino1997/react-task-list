7f118efa2c477b5e8571cb21c4a0e145f609a026
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-26 18:43:44 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import './App.css'
 import { tasks } from './assets/tasks'
 
 function App() {
-
+  const currentTasks = tasks.filter(task => task.state === "backlog" || task.state === "in_progress");
+  const completedTasks = tasks.filter(task => task.state === "completed");
   return (
     <>
       <h1>Task Manager</h1>
       <strong>Current Tasks(4)</strong>
       <ul>
-        {tasks.filter(task => task.state === "backlog" || task.state === "in_progress").map(el => (
+        {currentTasks.map(el => (
           <li key={el.id}>
-            <h2>{el.title}</h2>
+            <h2>{el.title} <em>{el.state}</em></h2>
+            <p>Priority:{el.priority}</p>
+            <p>Est. time:{el.estimatedTime}</p>
+          </li>
+        ))}
+      </ul>
+      <strong>Completed Tasks(6)</strong>
+      <ul>
+        {completedTasks.map(el => (
+          <li key={el.id}>
+            <h2>{el.title} <em>{el.state}</em></h2>
             <p>Priority:{el.priority}</p>
             <p>Est. time:{el.estimatedTime}</p>
           </li>
